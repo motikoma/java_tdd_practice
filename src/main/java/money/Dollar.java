2f86660c3d3f8d7f358e517a2c7332f9cb4c2ac2
坂本圭佑 <keisuke_sakamoto@sakamotokeisukenoMacBook-Pro.local>
2021-07-15 22:40:29 +0900

    第6章 テスト不足に気付いたら

--- src/main/java/money/Dollar.java
+++ src/main/java/money/Dollar.java
@@ -5,9 +5,7 @@ import java.util.Objects;
 // バリューオブジェクト：コンストラクタで設定したインスタンス変数の値が変わらない
 // バリューオブジェクトを使えば別名参照問題を防ぐことができる
 // フィールドをprivateにしておくことで間違った代入を防ぐことができる
-public class Dollar {
-
-  private int amount;
+public class Dollar extends Money {
 
   Dollar(int amount) {
     this.amount = amount;
@@ -15,18 +13,5 @@ public class Dollar {
 
   Dollar times(int multiplier) {
     return new Dollar(this.amount * multiplier);
-  }
-
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
-    Dollar dollar = (Dollar) o;
-    return amount == dollar.amount;
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(amount);
   }
 }
--- src/main/java/money/Franc.java
+++ src/main/java/money/Franc.java
@@ -2,9 +2,7 @@ package money;
 
 import java.util.Objects;
 
-public class Franc {
-
-  private int amount;
+public class Franc extends Money{
 
   Franc(int amount) {
     this.amount = amount;
@@ -12,18 +10,5 @@ public class Franc {
 
   Franc times(int multiplier) {
     return new Franc(this.amount * multiplier);
-  }
-
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
-    Franc franc = (Franc) o;
-    return amount == franc.amount;
-  }
-
-  @Override
-  public int hashCode() {
-    return Objects.hash(amount);
   }
 }
