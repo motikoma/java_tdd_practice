dbc140742afab6a92aed994d64d3baf691bec88b
坂本圭佑 <keisuke_sakamoto@sakamotokeisukenoMacBook-Pro.local>
2021-07-19 21:49:22 +0900

    第12章 設計とメタファー

--- src/main/java/money/Money.java
+++ src/main/java/money/Money.java
@@ -2,49 +2,52 @@ package money;
 
 import java.util.Objects;
 
-class Money {
-    protected int amount;
-    protected String currency;
+class Money implements Expression {
+  protected int amount;
+  protected String currency;
 
-    Money (int amount, String currency) {
-        this.amount = amount;
-        this.currency = currency;
-    }
+  Money(int amount, String currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
 
-    String currency(){
-        return currency;
-    };
+  String currency() {
+    return currency;
+  }
+  ;
 
-    Money times(int multiplier) {
-        return new Money(this.amount * multiplier, currency);
-    }
+  Money times(int multiplier) {
+    return new Money(this.amount * multiplier, currency);
+  }
 
-    static Money dollar(int amount){
-        return new Money(amount, "USD");
-    };
+  Expression plus(Money addend) {
+    return new Money(amount + addend.amount, currency);
+  }
 
-    static Money franc(int amount){
-        return new Money(amount, "CHF");
-    }
+  static Money dollar(int amount) {
+    return new Money(amount, "USD");
+  }
+  ;
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Money money = (Money) o;
-        return amount == money.amount && currency.equals(money.currency);
-    }
+  static Money franc(int amount) {
+    return new Money(amount, "CHF");
+  }
 
-    @Override
-    public int hashCode() {
-        return Objects.hash(amount);
-    }
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    Money money = (Money) o;
+    return amount == money.amount && currency.equals(money.currency);
+  }
 
-    @Override
-    public String toString() {
-        return "Money{" +
-                "amount=" + amount +
-                ", currency='" + currency + '\'' +
-                '}';
-    }
+  @Override
+  public int hashCode() {
+    return Objects.hash(amount);
+  }
+
+  @Override
+  public String toString() {
+    return "Money{" + "amount=" + amount + ", currency='" + currency + '\'' + '}';
+  }
 }
--- src/test/java/money/MoneyTest.java
+++ src/test/java/money/MoneyTest.java
@@ -16,24 +16,34 @@ import static org.junit.jupiter.api.Assertions.*;
 // [] testFrancMultiplicationを削除する？
 
 public class MoneyTest {
-    @Test
-    public void testMultiplication() throws Exception {
-        Money five = Money.dollar(5);
-        assertEquals(Money.dollar(10), five.times(2));
-        assertEquals(Money.dollar(15), five.times(3));
-    }
-    @Test
-    public void testEquality(){
-        assertTrue(Money.dollar(5).equals(Money.dollar(5)));
-        assertFalse(Money.dollar(5).equals(Money.dollar(6)));
-        System.out.println(Money.dollar(5).currency);
-        System.out.println(Money.franc(5).currency);
-        assertFalse(Money.franc(5).equals(Money.dollar(5)));
-    }
+  @Test
+  public void testMultiplication() throws Exception {
+    Money five = Money.dollar(5);
+    assertEquals(Money.dollar(10), five.times(2));
+    assertEquals(Money.dollar(15), five.times(3));
+  }
 
-    @Test
-    public void testCurrency(){
-        assertEquals("USD", Money.dollar(1).currency());
-        assertEquals("CHF", Money.franc(1).currency());
-    }
+  @Test
+  public void testEquality() {
+    assertTrue(Money.dollar(5).equals(Money.dollar(5)));
+    assertFalse(Money.dollar(5).equals(Money.dollar(6)));
+    System.out.println(Money.dollar(5).currency);
+    System.out.println(Money.franc(5).currency);
+    assertFalse(Money.franc(5).equals(Money.dollar(5)));
+  }
+
+  @Test
+  public void testCurrency() {
+    assertEquals("USD", Money.dollar(1).currency());
+    assertEquals("CHF", Money.franc(1).currency());
+  }
+
+  @Test
+  public void testSimpleAddition() {
+    Money five = Money.dollar(5);
+    Expression sum = five.plus(five);
+    Bank bank = new Bank();
+    Money reduced = bank.reduce(sum, "USD");
+    assertEquals(Money.dollar(10), reduced);
+  }
 }
