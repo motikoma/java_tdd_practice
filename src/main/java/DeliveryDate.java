4d9fd43cc567385f1997b8214bfb810be5248169
sakamoto keisuke <motikoma5125@gmail.com>
2022-03-07 20:37:06 +0900

    fix: リファクタリング

--- src/main/java/DeliveryDate.java
+++ src/main/java/DeliveryDate.java
@@ -1,34 +1,39 @@
 import java.time.LocalDate;
 import java.time.Month;
+import java.util.List;
 
 
 // 現在日時を取得して、月末を配送日として設定する
 // 現在日時が下旬や年末の場合は、配送日が次月に設定される
 
 public class DeliveryDate {
-    public LocalDate getDeliveryDate(){
-        LocalDate localDate = LocalDate.now();
+    public LocalDate getDeliveryDate() {
+        LocalDate localDate = getNow();
         int day = localDate.getDayOfMonth();
         Month month = localDate.getMonth();
         int year = localDate.getYear();
 
-        if(day >= 25){
+        if (day >= 25) {
             month.plus(1L);
-        } else if(month.equals(Month.DECEMBER) && day >= 20){
+        } else if (month.equals(Month.DECEMBER) && day >= 20) {
             month.plus(1L);
         }
 
+
+
+        return LocalDate.of(localDate.getYear(), localDate.getMonth(), lastDay);
+    }
+
+    protected int getLastDay(int year, Month month){
         int lastDay;
-        if(month.equals(Month.APRIL)){
+        if(List.of(Month.APRIL, Month.JUNE, Month.SEPTEMBER, Month.NOVEMBER).contains(month)){
             lastDay = 30;
-        } else if(month.equals(Month.JUNE)){
-            lastDay = 30;
-        } else if(month.equals(Month.SEPTEMBER)){
-            lastDay = 30;
-        } else if(month.equals(Month.NOVEMBER)){
-            lastDay = 30;
-        } else if(month.equals(Month.FEBRUARY)){
-            if(year % 4 == 0){
+        } else if (month.equals(Month.FEBRUARY)) {
+            if (year % 400 == 0) {
+                lastDay = 29;
+            } else if (year % 100 == 0) {
+                lastDay = 28;
+            } else if (year % 4 == 0) {
                 lastDay = 29;
             } else {
                 lastDay = 28;
@@ -36,6 +41,11 @@ public class DeliveryDate {
         } else {
             lastDay = 31;
         }
-        return LocalDate.of(localDate.getYear(), localDate.getMonth(), lastDay);
+
+        return lastDay;
+    }
+
+    protected LocalDate getNow(){
+        return LocalDate.now();
     }
 }
---
+++ src/test/java/DeliveryDateTest.java
@@ -0,0 +1,99 @@
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import static org.junit.jupiter.api.Assertions.*;
+
+class DeliveryDateTest {
+
+    @Nested
+    class _2月の月末 {
+        @Test
+        void _4で割り切れない年の2月の月末になる場合のテスト(){
+            FakeDeliveryDate deliveryDate = new FakeDeliveryDate();
+            deliveryDate.setDate(LocalDate.of(2019,2,13));
+            LocalDate actualDate = deliveryDate.getDeliveryDate();
+            assertEquals(LocalDate.of(2019,2,28), actualDate);
+        }
+
+        @Test
+        void _4で割り切れるが100で割り切れない年の2月の月末になる場合のテスト(){
+            FakeDeliveryDate deliveryDate = new FakeDeliveryDate();
+            deliveryDate.setDate(LocalDate.of(2020,2,13));
+            LocalDate actualDate = deliveryDate.getDeliveryDate();
+            assertEquals(LocalDate.of(2020,2,29), actualDate);
+        }
+
+        @Test
+        void _100で割り切れるが400で割り切れない年の2月の月末になる場合のテスト(){
+            FakeDeliveryDate deliveryDate = new FakeDeliveryDate();
+            deliveryDate.setDate(LocalDate.of(2100,2,13));
+            LocalDate actualDate = deliveryDate.getDeliveryDate();
+            assertEquals(LocalDate.of(2100,2,28), actualDate);
+        }
+
+        @Test
+        void _400で割り切れる年の2月の月末になる場合のテスト(){
+            FakeDeliveryDate deliveryDate = new FakeDeliveryDate();
+            deliveryDate.setDate(LocalDate.of(2000,2,13));
+            LocalDate actualDate = deliveryDate.getDeliveryDate();
+            assertEquals(LocalDate.of(2000,2,29), actualDate);
+        }
+    }
+
+    @Nested
+    class _小の月の月末 {
+        @Test
+        void _4月の月末の日付のテスト(){
+            DeliveryDate deliveryDate = new FakeDeliveryDate();
+            ((FakeDeliveryDate) deliveryDate).setDate(LocalDate.of(2020, 4, 13));
+            LocalDate actualDate = deliveryDate.getDeliveryDate();
+            assertEquals(LocalDate.of(2020,4,30), actualDate);
+        }
+
+        @Test
+        void _6月の月末の日付のテスト(){
+            DeliveryDate deliveryDate = new FakeDeliveryDate();
+            ((FakeDeliveryDate) deliveryDate).setDate(LocalDate.of(2020, 6, 13));
+            LocalDate actualDate = deliveryDate.getDeliveryDate();
+            assertEquals(LocalDate.of(2020,6,30), actualDate);
+        }
+
+        @Test
+        void _9月の月末の日付のテスト(){
+            DeliveryDate deliveryDate = new FakeDeliveryDate();
+            ((FakeDeliveryDate) deliveryDate).setDate(LocalDate.of(2020, 9, 13));
+            LocalDate actualDate = deliveryDate.getDeliveryDate();
+            assertEquals(LocalDate.of(2020,9,30), actualDate);
+        }
+
+        @Test
+        void _11月の月末の日付のテスト(){
+            DeliveryDate deliveryDate = new FakeDeliveryDate();
+            ((FakeDeliveryDate) deliveryDate).setDate(LocalDate.of(2020, 11, 13));
+            LocalDate actualDate = deliveryDate.getDeliveryDate();
+            assertEquals(LocalDate.of(2020,11,30), actualDate);
+        }
+    }
+
+    @Test
+    void _大の月の月末の日付のテスト(){
+        DeliveryDate deliveryDate = new FakeDeliveryDate();
+        ((FakeDeliveryDate) deliveryDate).setDate(LocalDate.of(2020, 10, 13));
+        LocalDate actualDate = deliveryDate.getDeliveryDate();
+        assertEquals(LocalDate.of(2020,10,31), actualDate);
+    }
+
+    private class FakeDeliveryDate extends DeliveryDate {
+        private LocalDate date;
+
+        @Override
+        protected LocalDate getNow() {
+            return date;
+        }
+
+        void setDate(LocalDate date) {
+            this.date = date;
+        }
+    }
+}
