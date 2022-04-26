fb02bf6a88a7df128291a7337393b57d8e9aadc8
sakamoto keisuke <motikoma5125@gmail.com>
2022-04-26 21:47:46 +0900

    add: Equipments

---
+++ src/main/java/immutable/equipment/Equipment.java
@@ -0,0 +1,18 @@
+package immutable.equipment;
+
+public class Equipment {
+    public static final Equipment EMPTY = new Equipment("", 0, 0, 0);
+    private final String name;
+    private final int cost;
+    private final int defence;
+    private final int magicDefence;
+
+    public Equipment(final String name, final int cost, final int defence, final int magicDefence) {
+        this.name = name;
+        this.cost = cost;
+        this.defence = defence;
+        this.magicDefence = magicDefence;
+    }
+}
+
+
